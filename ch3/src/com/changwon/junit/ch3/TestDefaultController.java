7c03e280ad895b30d314ec1d3a5c3016bc5e08e8
changwon <thswave@gmail.com>
2014-01-12 19:02:10 +0900

    add error handle test

--- ch3/src/com/changwon/junit/ch3/TestDefaultController.java
+++ ch3/src/com/changwon/junit/ch3/TestDefaultController.java
@@ -1,6 +1,8 @@
 package com.changwon.junit.ch3;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
 
 import org.junit.Before;
 import org.junit.Ignore;
@@ -8,7 +10,6 @@ import org.junit.Test;
 
 public class TestDefaultController {
 
-	@SuppressWarnings("unused")
 	private DefaultController controller;
 	private Request request;
 	private RequestHandler handler;
@@ -43,13 +44,35 @@ public class TestDefaultController {
 		assertEquals("Response should be of type SampleResponse",
 				SampleResponse.class, response.getClass());
 	}
+	
+	@Test
+	public void testProcessAnswersErrorsResponse(){
+		SampleRequest request = new SampleRequest("testError");
+		SampleExceptionHandler handler = new SampleExceptionHandler();
+		controller.addHandler(request, handler);
+		Response response = controller.processRequest(request);
+		assertNotNull("Must not return a null response", response);
+		assertEquals(ErrorResponse.class, response.getClass());
+	}
 
 	private class SampleRequest implements Request {
 
+		private static final String NAME = "Test";
+		private String name;
+		
+		public SampleRequest(){
+			this(NAME);
+		}
+		
+		public SampleRequest(String name){
+			this.name = name;
+		}
+
 		@Override
 		public String getName() {
-			return "Test";
+			return this.name;
 		}
+		
 	}
 
 	private class SampleHandler implements RequestHandler {
@@ -61,6 +84,35 @@ public class TestDefaultController {
 	}
 
 	private class SampleResponse implements Response {
+		private static final String NAME = "Test";
+		
+		public SampleResponse(){}
+		
+		public String getName(){
+			return NAME;
+		}
+		
+		@Override
+		public boolean equals(Object object){
+			boolean result = false;
+			if (object instanceof SampleResponse){
+				result = ((SampleResponse) object).getName().equals(getName());
+			}
+			return result;
+		}
+		
+		@Override
+		public int hashCode(){
+			return NAME.hashCode();
+		}
+	}
+	
+	private class SampleExceptionHandler implements RequestHandler{
 
+		@Override
+		public Response process(Request request) throws Exception {
+			throw new Exception("error processing request");
+		}
+		
 	}
 }
