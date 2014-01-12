89be7fbfdb36a2ce623fd339505b3b46805c637b
changwon <thswave@gmail.com>
2014-01-12 18:00:12 +0900

    convert junit test

--- ch1/src/com/changwon/junit/ch1/CalculatorTest.java
+++ ch1/src/com/changwon/junit/ch1/CalculatorTest.java
@@ -1,30 +1,15 @@
 package com.changwon.junit.ch1;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class CalculatorTest {
 
-	private int numberErrors = 0;
-
-	public void testAdd() {
+	@Test
+	public void testAdd(){
 		Calculator calculator = new Calculator();
 		double result = calculator.add(10, 50);
-		if (result != 60) {
-			throw new IllegalStateException("Bad result:" + result);
-		}
-
-	}
-
-	public static void main(String[] args) {
-		CalculatorTest test = new CalculatorTest();
-		try {
-			test.testAdd();
-		} catch (Throwable e) {
-			test.numberErrors++;
-			e.printStackTrace();
-		}
-		if (test.numberErrors > 0) {
-			throw new IllegalStateException("There were " + test.numberErrors
-					+ " error(s)");
-		}
-
+		assertEquals(60, result, 0);
 	}
 }
